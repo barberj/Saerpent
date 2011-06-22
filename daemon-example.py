a673c1f4c3b93583215037f0fdc33c7d53e9b220
barberj <barber.justin@gmail.com>
2011-06-22 17:35:46 -0400

    Tinkering with creating my own python daemon with logging

---
+++ daemon-example.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+import sys, time
+from daemon import Daemon
+
+import logging
+log = logging.getLogger()
+
+from datetime import datetime
+
+class MyDaemon(Daemon):
+	def run(self):
+		while True:
+			log.info(datetime.now())
+			time.sleep(1)
+
+if __name__ == "__main__":
+	logger = logging.getLogger()
+	logger.setLevel(logging.DEBUG)
+	handler = logging.FileHandler('/tmp/daemon.log')
+	formatter = logging.Formatter("%(asctime)-15s %(name)s: %(message)s")
+	handler.setFormatter(formatter)
+	logger.addHandler(handler)
+
+	daemon = MyDaemon('/tmp/daemon-example.pid')
+	if len(sys.argv) == 2:
+		logger.info('%s %s',sys.argv[1],sys.argv[0])
+		if 'start' == sys.argv[1]:
+			daemon.start()
+		elif 'stop' == sys.argv[1]:
+			daemon.stop()
+		elif 'restart' == sys.argv[1]:
+			daemon.restart()
+		elif 'status' == sys.argv[1]:
+			daemon.status()
+		else:
+			print "Unknown command"
+			sys.exit(2)
+		sys.exit(0)
+	else:
+		logger.warning('displaying usage')
+		print "usage: %s start|stop|restart" % sys.argv[0]
+		sys.exit(2)
