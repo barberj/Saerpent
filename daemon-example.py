f73a6078326a521aeb4a844e78fbff2a39bd65bc
barberj <barber.justin@gmail.com>
2011-06-23 16:07:04 -0400

    converted tabs to spaces

--- daemon-example.py
+++ daemon-example.py
@@ -15,22 +15,22 @@ from mydaemon import MyDaemon
 
 
 if __name__ == "__main__":
-	daemon = MyDaemon('/tmp/daemon-example.pid')
-	if len(sys.argv) == 2:
-		logger.info('%s %s',sys.argv[1],sys.argv[0])
-		if 'start' == sys.argv[1]:
-			daemon.start()
-		elif 'stop' == sys.argv[1]:
-			daemon.stop()
-		elif 'restart' == sys.argv[1]:
-			daemon.restart()
-		elif 'status' == sys.argv[1]:
-			daemon.status()
-		else:
-			print "Unknown command"
-			sys.exit(2)
-		sys.exit(0)
-	else:
-		logger.warning('displaying usage')
-		print "usage: %s start|stop|restart" % sys.argv[0]
-		sys.exit(2)
+    daemon = MyDaemon('/tmp/daemon-example.pid')
+    if len(sys.argv) == 2:
+        logger.info('%s %s',sys.argv[1],sys.argv[0])
+        if 'start' == sys.argv[1]:
+            daemon.start()
+        elif 'stop' == sys.argv[1]:
+            daemon.stop()
+        elif 'restart' == sys.argv[1]:
+            daemon.restart()
+        elif 'status' == sys.argv[1]:
+            daemon.status()
+        else:
+            print "Unknown command"
+            sys.exit(2)
+        sys.exit(0)
+    else:
+        logger.warning('displaying usage')
+        print "usage: %s start|stop|restart" % sys.argv[0]
+        sys.exit(2)
--- mydaemon.py
+++ mydaemon.py
@@ -8,8 +8,8 @@ log = logging.getLogger(__name__)
 from datetime import datetime
 
 class MyDaemon(Daemon):
-	def run(self):
-		while True:
-			log.info('TOP %s', datetime.now())
-			time.sleep(60)
-			log.info('ROLLING %s', datetime.now())
+    def run(self):
+        while True:
+            log.info('TOP %s', datetime.now())
+            time.sleep(60)
+            log.info('ROLLING %s', datetime.now())
