f87238d55693e3b21d9a5eca4eee83e3ecb6f196
barberj <barber.justin@gmail.com>
2011-06-23 15:59:48 -0400

    logger changes. curious.

--- daemon-example.py
+++ daemon-example.py
@@ -1,21 +1,20 @@
 #!/usr/bin/env python
+import logging
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+handler = logging.FileHandler('/tmp/daemon.log')
+formatter = logging.Formatter("%(asctime)-15s %(name)s: %(message)s")
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
 import sys, time
+from datetime import datetime
+
 from daemon import Daemon
 from mydaemon import MyDaemon
 
-import logging
-log = logging.getLogger(__name__)
-
-from datetime import datetime
 
 if __name__ == "__main__":
-	logger = logging.getLogger()
-	logger.setLevel(logging.DEBUG)
-	handler = logging.FileHandler('/tmp/daemon.log')
-	formatter = logging.Formatter("%(asctime)-15s %(name)s: %(message)s")
-	handler.setFormatter(formatter)
-	logger.addHandler(handler)
-
 	daemon = MyDaemon('/tmp/daemon-example.pid')
 	if len(sys.argv) == 2:
 		logger.info('%s %s',sys.argv[1],sys.argv[0])
