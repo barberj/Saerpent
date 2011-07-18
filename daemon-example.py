645dc1c6117615d4067e218e0a6cddf603113314
barberj <barber.justin@gmail.com>
2011-07-18 13:41:25 -0400

    update formater. syslog handles timestamp.

--- daemon-example.py
+++ daemon-example.py
@@ -9,13 +9,10 @@ formatter = logging.Formatter("%(asctime)-15s %(name)s: %(message)s")
 filehandler.setFormatter(formatter)
 logger.addHandler(filehandler)
 
-#sysloghandler = logging.handlers.SysLogHandler('ec2-50-16-173-176.compute-1.amazonaws.com',514)
-#sysloghandler = logging.handlers.SysLogHandler('10.36.78.6',514)
-#sysloghandler = logging.handlers.SysLogHandler('localhost',514)
-#sysloghandler = logging.handlers.SysLogHandler('127.0.0.1',514)
 # TY!!! http://scottbarnham.com/blog/2008/01/01/sysloghandler-not-writing-to-syslog-with-python-loggin
 sysloghandler = logging.handlers.SysLogHandler('/dev/log',514)
 
+formatter = logging.Formatter("%(name)s: %(message)s")
 sysloghandler.setFormatter(formatter)
 logger.addHandler(sysloghandler)
 
